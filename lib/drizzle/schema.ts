8f2d5b599a156bec9240c21a84249024abcdbc52
Chiboy <chiboyekene1@gmail.com>
2025-03-01 16:33:16 +0100

    xvxbxcb

--- lib/drizzle/schema.ts
+++ lib/drizzle/schema.ts
@@ -1,13 +1,14 @@
 import { integer, json, numeric, pgEnum, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core'
 
-export const account_type = pgEnum('account_type', ['student', 'vendor', 'owner'])
+export const accountType = pgEnum('account_type', ['student', 'vendor', 'owner'])
+export const stayDuration = pgEnum('stay_duration', ['day', 'month', 'year'])
 
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
     fullName: varchar('full_name', { length: 200 }).notNull(),
     amount: numeric('wallet_amount', { scale: 2 }).default("0.00").notNull(),
     email: varchar('email_address', { length: 100 }).notNull(),
-    account_type: account_type('account_type').notNull(),
+    account_type: accountType('account_type').notNull(),
     institutionId: integer('institution_id'),
     phone: varchar('phone_number', { length: 20 }).notNull(),
     propertyId: integer('property_id').references(() => property.id)
@@ -17,6 +18,7 @@ export const buildings = pgTable('buildings', {
     id: serial('id').primaryKey(),
     userId: integer('user_id').references(() => users.id),
     name: text('name').notNull().unique(),
+    description: text('description').notNull(),
     price: numeric('price', { scale: 2 }).notNull(),
     images: json('images').$type<string[]>().default([]).notNull()
 })
