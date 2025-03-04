aac17af9461598d3911c7fa7cc3f920961de527b
Chiboy <chiboyekene1@gmail.com>
2025-03-04 02:41:28 +0100

    Next

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ const Page = () => {
 		<main>
 			<Hero searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-				<div className="grid grid-cols-[repeat(auto-fit, minmax(300px, 1fr))] gap-6">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 					{filteredItems.map((val) => (
 						<Hostel key={val.id} {...val} />
 					))}
